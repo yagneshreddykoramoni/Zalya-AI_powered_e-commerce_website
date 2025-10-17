bef0ce890609599b890bf9f3a0e516cf6dc9aa6c
yagneshreddykoramoni <yagneshreddykoramoni@gmail.com>
2025-10-17 19:00:57 +0530

    edited index.js

--- server/index.js
+++ server/index.js
@@ -81,7 +81,7 @@ app.use('/api/style-suggestions', require('./routes/styleSuggestions'));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  app.get('*', (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 }
